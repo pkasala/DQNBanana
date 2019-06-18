f74a3fb72c44d0a1df4f29c99b5d2987a544d5c2
pkasala <p.kasala@gmail.com>
2019-06-18 08:17:36 +0200

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed):
+        super(QNetwork, self).__init__()
+        #self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size,64)
+        self.fc2 = nn.Linear(64,64)
+        self.fc3 = nn.Linear(64, action_size)
+
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        out=self.fc3(x)
+        return out
